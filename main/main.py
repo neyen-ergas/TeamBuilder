c24dc53e8a2d6a37144d47c3a460b3220e4a681a
dilllon <102103822+dilllon@users.noreply.github.com>
2025-04-21 14:05:40 -0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,4 +1,14 @@
+
+
 def main():
-    print("Hola mundo")
+    print("------Bienvenido a la aplicación de TeamBuilder------")
+    print("¿Que acción quiere realizar?")
+    print("1. Alta de jugador")
+    print("2. Baja de jugador")
+    print("3. Listado de jugadores")
+    print("4. Armar equipos")
+    print("5. Salir")
+
+    n = int(input("Ingresar acción: "))
 
 main()
