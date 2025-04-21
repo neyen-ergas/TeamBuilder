0776e75aaccfd65c1a2a2b60a74482dfc69dfbb3
Lucio Dillon <102103822+dilllon@users.noreply.github.com>
2025-04-21 14:45:55 -0300

    mejoras menu

--- main/main.py
+++ main/main.py
@@ -1,8 +1,13 @@
 
-
 def menuCrearPartido():
     #TODO print("¿Que plantilla quiere usar?")
     print("¿De cuantos jugadores van a ser los equipos?") #doy opciones? dejo que el usuario elija y desp checkeo?
+    print("¿5, 6, 7, 8, 9, 10 u 11?")
+    num = int(input("Ingresar cantidad de jugadores por equipo: "))
+    while num < 5 or num > 11:
+        print("Número no válido.")
+        num = int(input("Ingresar cantidad de jugadores por equipo: "))
+
     #TODO algoritmo de seleccion de jugadores
 
 def menuJugadores():
@@ -32,6 +37,7 @@ def main():
         menuJugadores()
     else:
         menuEstadisticas()
+    
     
 
     
