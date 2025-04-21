a7db13f64d8414aa2285de012326ffb4ba003862
dilllon <102103822+dilllon@users.noreply.github.com>
2025-04-21 14:29:48 -0300

    Mejor menu

--- main/main.py
+++ main/main.py
@@ -1,14 +1,37 @@
 
 
+def menuCrearPartido():
+    #TODO print("¿Que plantilla quiere usar?")
+    print("¿De cuantos jugadores van a ser los equipos?") #doy opciones? dejo que el usuario elija y desp checkeo?
+    #TODO algoritmo de seleccion de jugadores
+
+def menuJugadores():
+    print("¿Que acción quiere realizar?") #seleccionar plantilla??
+    print("1. Alta de jugador")
+    print("2. Editar jugador")
+    print("3 Baja de jugador")
+
+def menuEstadisticas():
+    print("¿De qué quiere ver estadísticas?")
+    print("1. Estadísticas de jugadores")
+    print("2. Estadísticas de plantilla") #????
+
 def main():
     print("------Bienvenido a la aplicación de TeamBuilder------")
     print("¿Que acción quiere realizar?")
-    print("1. Alta de jugador")
-    print("2. Baja de jugador")
-    print("3. Listado de jugadores")
-    print("4. Armar equipos")
-    print("5. Salir")
+    print("1. Crear Partido")
+    print("2. Administración de jugadores")
+    print("3. Estadísticas")
+    print("4. Salir")
 
     n = int(input("Ingresar acción: "))
 
-main()
+    if n == 1:
+        menuCrearPartido()
+    elif n == 2:
+        menuJugadores()
+    else:
+        menuEstadisticas()
+    
+
+    
