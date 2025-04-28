d009eed7aba43f4f5d6e57b9abbc549479a79615
ivorapi <ivanrapisarda7@gmail.com>
2025-04-28 19:18:00 -0300

    agrego documentación de funciones

--- functions/armado/agregarPrimerizos.py
+++ functions/armado/agregarPrimerizos.py
@@ -1,6 +1,23 @@
 import random
 
 def asignarPrimerizos(equipo1, equipo2, primerizos):
+
+    """
+    Distribuye aleatoriamente jugadores primerizos entre dos equipos de forma alternada.
+
+    Args:
+        equipo1 (list): Lista inicial para el primer equipo.
+        equipo2 (list): Lista inicial para el segundo equipo.
+        primerizos (list): Lista de jugadores primerizos a repartir.
+
+    Returns:
+        tuple: Dos listas actualizadas (equipo1, equipo2) con los jugadores distribuidos.
+
+    Notas:
+        - Los jugadores primerizos se mezclan aleatoriamente antes de ser asignados.
+        - Se asignan alternadamente basándose en el índice.
+    """
+    
     random.shuffle(primerizos) 
 
     for i, jugador in enumerate(primerizos):
